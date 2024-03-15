8cc5397f777be41d78fb81ede87743ab9595d12d
lam hung <hunglam.dev@gmail.com>
2024-03-15 17:56:20 +0700

    #2 handle error

---
+++ server/routes/index.js
@@ -0,0 +1,8 @@
+const userRouter = require("./user");
+const { notFound, errHandler } = require("../middlewares/errHandler");
+const initRouter = (app) => {
+  app.use("/api/user", userRouter);
+  app.use(notFound);
+  app.use(errHandler);
+};
+module.exports = initRouter;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 require("dotenv").config();
+const dbConennct = require("./config/dbconnect");
+const initRouter = require("./routes");
 
 const app = express();
 const port = process.env.PORT || 8888;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+dbConennct();
+initRouter(app);
 
 app.use("/", (req, res) => {
   res.send("SERVER ONNNNN");
